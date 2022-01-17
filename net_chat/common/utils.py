475dbacf40da9e4505c9ea1a197b37dfa35b3507
KstanislavK <t7s9SF2FWTQfXxL>
2022-01-17 11:22:36 +0300

    lesson 2_5

--- net_chat/common/utils.py
+++ net_chat/common/utils.py
@@ -1,33 +1,28 @@
 import json
-import sys
+from .decos import log
+from .params import *
 
-from params import ENCODING, MAX_PACKAGE_LENGTH
-from errors import IncorrectDataRecievedError, NonDictInputError
-
-sys.path.append('../')
-from decos import log
+# Утилита приёма и декодирования сообщения
+# принимает байты выдаёт словарь, если приняточто-то другое отдаёт ошибку типа
 
 
 @log
 def get_message(client):
-    """Прием и декодирование сообщения"""
     encoded_response = client.recv(MAX_PACKAGE_LENGTH)
-    if isinstance(encoded_response, bytes):
-        json_response = encoded_response.decode(ENCODING)
-        response = json.loads(json_response)
-        if isinstance(response, dict):
-            return response
-        else:
-            raise IncorrectDataRecievedError
+    json_response = encoded_response.decode(ENCODING)
+    response = json.loads(json_response)
+    if isinstance(response, dict):
+        return response
     else:
-        raise IncorrectDataRecievedError
+        raise TypeError
+
+
+# Утилита кодирования и отправки сообщения
+# принимает словарь и отправляет его
 
 
 @log
 def send_message(sock, message):
-    """Кодирования и отправка сообщения"""
-    if not isinstance(message, dict):
-        raise NonDictInputError
     js_message = json.dumps(message)
     encoded_message = js_message.encode(ENCODING)
     sock.send(encoded_message)
